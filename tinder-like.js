760791048c3c15cb1940c9073cc657d4ab5c1d1e
B.O.B <31910797+berkorhanbeta@users.noreply.github.com>
2020-11-02 23:34:17 +0300

    Update tinder-like.js

--- tinder-like.js
+++ tinder-like.js
@@ -1,10 +1,14 @@
 // Timer for like button.
 // 1000 = 1 second
 // 5000 = 5 second etc.
-var timer = 1000;
+// I recommend using a 2 second timer. If you set it for a shorter time (I don't recommend), Tinder can notice this and banned your account permanently.
+var timer = 2000;
 
 
 sendIt = setInterval(function(){
-  var begen = document.querySelectorAll("[aria-label='Beğen']");
+  // If you are not using Tinder in English language, then change the 'Like' text in your local language.
+  // Ex : In Turkish like button name is 'Beğen'. So you need to edit this code like this;
+  // "[aria-label='Beğen']"
+  var begen = document.querySelectorAll("[aria-label='Like']");
   begen[0].click()
 }, timer)
